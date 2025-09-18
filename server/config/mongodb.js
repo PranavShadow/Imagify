86c943871933093aef0cb170d7f56e667d97b79a
PranavShadow <ompranav2003@gmail.com>
2025-09-18 10:14:25 +0530

    updated mongodb.js

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,24 +1,24 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async ()=> {
-//     mongoose.connection.on('connected', ()=> {
-//         console.log("Database Connected")
-//     })
-//     await mongoose.connect(`${process.env.MONGODB_URI}/imagify`)
-// }
-
-// export default connectDB;
-
 import mongoose from "mongoose";
 
 const connectDB = async ()=> {
     mongoose.connection.on('connected', ()=> {
         console.log("Database Connected")
     })
-    await mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
+    await mongoose.connect(`${process.env.MONGODB_URI}/imagify`)
 }
 
 export default connectDB;
+
+// import mongoose from "mongoose";
+
+// const connectDB = async ()=> {
+//     mongoose.connection.on('connected', ()=> {
+//         console.log("Database Connected")
+//     })
+//     await mongoose.connect(process.env.MONGODB_URI, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     })
+// }
+
+// export default connectDB;
